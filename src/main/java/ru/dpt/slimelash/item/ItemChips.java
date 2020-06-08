a8481dd1cc7458c80bae18223bba048f07bbe547
DartPower <SIMPSON-NEKEK@mail.ru>
2020-06-08 17:58:09 +0300

    Хехе

---
+++ src/main/java/ru/dpt/slimelash/item/ItemChips.java
@@ -0,0 +1,51 @@
+
+package ru.dpt.slimelash.item;
+
+import ru.dpt.slimelash.ElementsSlimeLash;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.Item;
+import net.minecraft.item.EnumAction;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+
+@ElementsSlimeLash.ModElement.Tag
+public class ItemChips extends ElementsSlimeLash.ModElement {
+	@GameRegistry.ObjectHolder("slimelash:chips")
+	public static final Item block = null;
+	public ItemChips(ElementsSlimeLash instance) {
+		super(instance, 9);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemFoodCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("slimelash:chips", "inventory"));
+	}
+	public static class ItemFoodCustom extends ItemFood {
+		public ItemFoodCustom() {
+			super(4, 0.3f, false);
+			setUnlocalizedName("chips");
+			setRegistryName("chips");
+			setCreativeTab(CreativeTabs.FOOD);
+			setMaxStackSize(64);
+		}
+
+		@Override
+		public EnumAction getItemUseAction(ItemStack par1ItemStack) {
+			return EnumAction.EAT;
+		}
+	}
+}
